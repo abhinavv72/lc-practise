9f38d962347f4c48d0f7c95df46707b0fb67782d
Abhinav <98042083+abhinavv72@users.noreply.github.com>
2023-04-03 11:21:33 +0530

    Time: 32 ms (34.62%), Space: 13.3 MB (67.57%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -8,46 +8,32 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+class cmp
+{
+    public :
+    bool operator() (ListNode *a, ListNode *b){
+        return a->val>b->val;
+    }
+};
 class Solution {
 public:
-    ListNode* merge(ListNode *left, ListNode *right) {
-        ListNode *dummy = new ListNode(-1);
-        ListNode *temp = dummy;
-        while (left != nullptr && right != nullptr) {
-            if (left -> val < right -> val) {
-                temp -> next = left;
-                temp = temp -> next;
-                left = left -> next;
-            }
-            else {
-                temp -> next = right;
-                temp = temp -> next;
-                right = right -> next;
-            }
-        }
-        while (left != nullptr) {
-            temp -> next = left;
-            temp = temp -> next;
-            left = left -> next;
-        }
-        while (right != nullptr) {
-            temp -> next = right;
-            temp = temp -> next;
-            right = right -> next;
-        }
-        return dummy -> next;
-    }
-    ListNode* mergeSort(vector<ListNode*>& lists, int start, int end) {
-        if (start == end) 
-            return lists[start];
-        int mid = start + (end - start) / 2;
-        ListNode *left = mergeSort(lists, start, mid);
-        ListNode *right = mergeSort(lists, mid + 1, end);
-        return merge(left, right);
-    }
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if (lists.size() == 0)
-            return nullptr;
-        return mergeSort(lists, 0, lists.size() - 1);
+      priority_queue<ListNode*, vector<ListNode*>,cmp>q;
+        ListNode* dummy = new ListNode(-1);
+        ListNode* tail = dummy;
+        for(int i=0; i<lists.size(); i++){
+            if(lists[i]!=NULL){
+                q.push(lists[i]);
+            }
+        }
+            while(q.size()){
+                ListNode* temp = q.top();
+                tail->next=temp; 
+                tail = temp;
+                q.pop();
+                if(temp->next!=NULL)
+                    q.push(temp->next);
+            }
+        return dummy->next;
     }
 };
