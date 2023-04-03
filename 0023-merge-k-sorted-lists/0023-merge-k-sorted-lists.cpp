1f81497dcc50a72450b59b6f26e0c7aad6eed422
Abhinav <98042083+abhinavv72@users.noreply.github.com>
2023-04-03 11:22:21 +0530

    Time: 12 ms (99.52%), Space: 13.4 MB (67.57%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -8,6 +8,28 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+//approach STEP 1: Create a new dummy node (ans). It will have the value of -1 and will point to NULL i.e.,
+
+/*C++ -> ListNode *ans = new ListNode(-1);
+
+Java -> ListNode ans = new ListNode(-1);
+
+STEP 2: Now, create a new node (tail). It will be a copy (deep copy) of ans node. The purpose of a tail node is to keep the track of the last node of the sorted merged Linked List.
+
+STEP 3: Further, we will create a priority queue (Min Heap) of linked list nodes (Min Heap which will contain nodes), where we will insert the first node of every Linked list present, in turn creating a priority queue of size k, k denotes the number of linked lists present.
+
+STEP 4: Now, we will remove the top node of the priority queue, store it in some temporary node(temp), and delete the top node of the priority queue.
+
+STEP 5: Attach the temp node to the next tail node.
+
+STEP 6: Further, make the tail pointer point to the temp node, indicating that temp is the last node of the sorted merged linked list formed so far.
+
+STEP 7: If the next node of temp is not NULL, then we will insert the next node of temp.
+
+STEP 8: Repeat step 5,6,7,8 till the priority queue is not empty.
+
+STEP 9: Finally, return the head of sorted linked list i.e. next of ans node since its next pointer points to the first node of merged sorted Linked List.*/
+
 class cmp
 {
     public :
